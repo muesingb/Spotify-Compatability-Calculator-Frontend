401c15bffa3f46021d12b93054ff273d3ea05e59
Barbara <muesingb@gmail.com>
2019-12-30 12:03:57 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Spotify from 'spotify-web-api-js';
 
-function App() {
-  return (
+const spotifyWebApi = new Spotify();
+const backEndLoginURI = "http://localhost:3000"
+
+class App extends React.Component {
+  constructor() {
+    super();
+    const params = this.getHashParams();
+    console.log(params)
+    this.state = {
+      loggedIn: params.access_token ? true : false
+    }
+    if (params.access_token) {
+      spotifyWebApi.setAccessToken(params.access_token)
+    }
+  }
+
+  getHashParams() {
+    var hashParams = {};
+    var e, r = /([^&;=]+)=?([^&;]*)/g,
+        q = window.location.hash.substring(1);
+    while ( e = r.exec(q)) {
+       hashParams[e[1]] = decodeURIComponent(e[2]);
+    }
+    return hashParams;
+  }
+
+  getMyTopArtists() {
+    spotifyWebApi.getMyTopArtists()
+      .then(data => {
+        console.log(data)
+        data.items.forEach(artist => console.log(artist.name))
+      })
+    spotifyWebApi.getMyTopTracks()
+      .then(data => {
+        console.log(data)
+        data.items.forEach(track => console.log(track.name))
+      })
+  }
+
+  render() {
+    console.log(this.state)
+    this.getMyTopArtists()
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href={backEndLoginURI} >
+      <button>Login With Spotify!</button>
+      </a>
+      <div>
+        Now playing {this.state.loggedIn ? "true" : "false"}
+      </div>
     </div>
   );
+  }
 }
 
 export default App;
